b32f2c91d196fed194abb6cc772cbade5048f920
GDM94 <giovanni.dimartino1994@gmail.com>
2020-12-14 09:53:07 +0100

    init db module

---
+++ src/main/java/com/example/demo2/DbModuleApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo2;
+
+import com.example.communication.CommunicationModuleApp;
+import com.example.communication.model.Anagrafica;
+import com.example.communication.model.Indirizzo;
+import org.springframework.boot.SpringApplication;
+
+
+public class DbModuleApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbModuleApplication.class, args);
+		Anagrafica anagrafica = new Anagrafica();
+
+
+
+	}
+
+}
